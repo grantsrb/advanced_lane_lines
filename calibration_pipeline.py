6b00d2c4f885634940da4f4efd20ba17658873f0
Satchel Grant <grantsrb@gmail.com>
2017-05-01 03:33:57 -0700

    test image transform

--- calibration_pipeline.py
+++ calibration_pipeline.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 import calibration as calib
+import lanes
+import filters
 
 
 def show(img):
@@ -11,14 +13,21 @@ def show(img):
     plt.show()
 
 cal_imgs_dir = 'camera_cal'
-img_type = '.jpg'
 nxy = (9,6) # tuple of chess board corners (across, vertical)
 offset = 50
 
-calibrators = calib.calibrate(cal_imgs_dir,img_type,nxy)
+calibrators = calib.calibrate(cal_imgs_dir,nxy)
 
-img = mpimg.imread('camera_cal/calibration9.jpg')
-show(img)
-M,_ = calib.get_transform(img,nxy,calibrators,offset)
+
+img = mpimg.imread('test_images/straight_lines2.jpg')
+img = calib.undistort(img,calibrators)
+filt_img = filters.filter(img)
+left_line, right_line = lanes.get_lane_lines(filt_img)
+src_pts = np.float32([[left_line[0],left_line[1]],
+                    [left_line[2],left_line[3]],
+                    [right_line[2],right_line[3]],
+                    [right_line[0],right_line[1]]])
+# src_pts = np.float32([[277,680],[555,475],[727,474],[1049,680]])
+M,M_rev = calib.get_transform(src_pts,img.shape)
 img = calib.change_persp(img,M)
 show(img)
