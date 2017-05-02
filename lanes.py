08a873119411025e49aa46e4ba3b06c45f3d8a93
Satchel Grant <grantsrb@gmail.com>
2017-05-01 23:29:30 -0700

    add processing functions for finding lanes, curvature, and plotting results

--- lanes.py
+++ lanes.py
@@ -62,15 +62,9 @@ def get_line(ptvec,img_size,h_ratio=40/64):
 def get_lane_lines(filtered_img):
     filt_left,filt_right = partition_lanes(filtered_img)
     left_lines = cv2.HoughLinesP(filt_left.astype(np.uint8),
-                                5,
-                                np.pi/180,
-                                20,
-                                np.array([]))
+                                5, np.pi/180, 20, np.array([]))
     right_lines = cv2.HoughLinesP(filt_right.astype(np.uint8),
-                                5,
-                                np.pi/180,
-                                20,
-                                np.array([]))
+                                5, np.pi/180, 20, np.array([]))
     leftptvec = ptvecs(left_lines)
     rightptvec = ptvecs(right_lines)
     left_line = get_line(leftptvec,filtered_img.shape)
@@ -80,3 +74,154 @@ def get_lane_lines(filtered_img):
 def draw_lanes(filtered_img):
     left_line,right_line = get_lane_lines(filtered_img)
     return draw_lines(np.zeros_like(filtered_img),[[left_line],[right_line]])
+
+def fit_lanes(birds_img, filt=True):
+    if filt:
+        sat_img = filters.get_saturation(birds_img)
+    else:
+        sat_img = birds_img
+    sat_img[sat_img > 0] = 1
+    hist = np.sum(sat_img[sat_img.shape[0]//2:,:],axis=0)
+    plt.plot(hist)
+
+def window_topbottom(img_size, n, height):
+    bottom = img_size[0] - (n+1) * height
+    top = img_size[0] - n * height
+    return bottom, top
+
+def window_walls(current_column, margin):
+    left_wall = current_column - margin
+    right_wall = current_column + margin
+    return left_wall, right_wall
+
+
+def find_lane_pixels(filt_img,n_windows=10,margin=100,minpixels=50):
+    filt_img[filt_img != 0] = 1
+    output = np.dstack([filt_img*255 for i in range(3)])
+    midpt = output.shape[1]//2
+
+    # Use histogram on image to find lane base locations
+    hist = np.sum(filt_img[filt_img.shape[0]//2:,:],axis=0)
+    left_cols_base = np.argmax(hist[:midpt])
+    right_cols_base = np.argmax(hist[midpt:]) + midpt
+
+    # Will make windows to isolate potential lane pixels
+    window_height = output.shape[0]//n_windows
+    nonzeros = filt_img.nonzero()
+    nz_rows, nz_cols = nonzeros[0], nonzeros[1]
+
+    left_current_column = left_cols_base
+    right_current_column = right_cols_base
+
+    left_lane_indices = []
+    right_lane_indices = []
+
+    for window in range(n_windows):
+        # Find window edges
+        win_bottom, win_top = window_topbottom(filt_img.shape,window,window_height)
+
+        left_lwall, left_rwall = window_walls(left_current_column, margin)
+        right_lwall, right_rwall = window_walls(right_current_column, margin)
+
+        # Draw the windows on the output
+        cv2.rectangle(output,(left_lwall,win_bottom),
+                            (left_rwall,win_top),
+                            (0,255,0), 2)
+        cv2.rectangle(output,(right_lwall,win_bottom),
+                            (right_rwall,win_top),
+                            (0,255,0), 2)
+
+        # Get indices of the nonzero pixels in within the window
+        nz_left_indices = ((nz_rows >= win_bottom) &
+                            (nz_rows < win_top) &
+                            (nz_cols >= left_lwall) &
+                            (nz_cols < left_rwall)).nonzero()[0]
+        nz_right_indices = ((nz_rows >= win_bottom) &
+                            (nz_rows < win_top) &
+                            (nz_cols >= right_lwall) &
+                            (nz_cols < right_rwall)).nonzero()[0]
+
+        # Append the nonzero indices to the index lists
+        left_lane_indices.append(nz_left_indices)
+        right_lane_indices.append(nz_right_indices)
+
+        # If more than minpixels are within a window, window recenters on mean
+        if len(nz_left_indices) > minpixels:
+            left_current_column = np.int(np.mean(nz_cols[nz_left_indices]))
+        if len(nz_right_indices) > minpixels:
+            right_current_column = np.int(np.mean(nz_cols[nz_right_indices]))
+
+    # Concatenate respective lane indices together into individual arrays
+    left_lane_indices = np.concatenate(left_lane_indices)
+    right_lane_indices = np.concatenate(right_lane_indices)
+
+    # Extract left and right lane pixel positions
+    l_nzlane_rowcols = nz_cols[left_lane_indices],
+                            nz_rows[left_lane_indices]
+    r_nzlane_rowcols = nz_cols[right_lane_indices],
+                            nz_rows[right_lane_indices]
+
+    return l_nzlane_rowcols, r_nzlane_rowcols
+
+def poly_params(nzlane_rowcols,degree=2):
+    rows, cols = nzlane_rowcols
+    fit = np.polyfit(rows, cols, degree)
+    return fit
+
+def fit_poly(img_size, nzlane_rowcols):
+    params = poly_params(nzlane_rowcols)
+    plot_rows = np.linspace(0, img_shape[0]-1, img_shape[0])
+    fit_cols = params[0]*plot_rows**2 + params[1]*plot_rows + params[2]
+    return plot_rows, fit_cols, params
+
+def update_nzindices(params, nz_cols, nz_rows, margin):
+    return ((nz_cols > (params[0]*nz_rows**2 +\
+                        params[1]*nz_rows + params[2] - margin)) &\
+                        (nz_cols < (params[0]*nz_rows**2 +\
+                        params[1]*nz_rows + params[2] + margin)))
+
+def find_lanes_update(filt_img, params, margin=100):
+    left_fit, right_fit = params
+
+    nonzeros = filt_img.nonzero()
+    nz_rows, nz_cols = nonzeros[0], nonzeros[1]
+
+    left_lane_indices = update_nzindices(left_fit, nz_cols, nz_rows, margin)
+    right_lane_indices = update_nzindices(right_fit, nz_cols, nz_rows, margin)
+
+    l_nzlane_rowcols = nz_rows[left_lane_indices],
+                            nz_cols[left_lane_indices]
+    r_nzlane_rowcols = nz_rows[right_lane_indices],
+                            nz_cols[right_lane_indices]
+
+    plot_rows, left_fit_cols, lparams = fit_poly(filt_img.shape, l_nzlane_rowcols)
+    plot_rows, right_fit_cols, rparams = fit_poly(filt_img.shape, r_nzlane_rowcols)
+
+    l_nzlane_rowcols = (left_rows,left_cols)
+    r_nzlane_rowcols = (right_rows,right_cols)
+    params = (lparams,rparams)
+    plots = (plot_rows,left_fit_cols,right_fit_cols)
+
+    return l_nzlane_rowcols, r_nzlane_rowcols, params, plots
+
+def curve_radius(eval_row, nzlane_rowcols, row_mpp=30/720, col_mpp=3.7/700):
+    nzlane_rowcols = (nzlane_rowcols[0]*row_mpp, nzlane_rowcols[1]*col_mpp)
+    eval_row = eval_row*row_mpp
+
+    fit_params = poly_params(nzlane_rowcols)
+
+    radius = ((1 + (2*fit_params[0]*eval_row +
+                    fit_params[1])**2)**1.5)/abs(2*fit_params[0])
+    return radius
+
+def show_lines(img, l_nzlane_rowcols, r_nzlane_rowcols):
+    plot_row, left_fit_col, _ = fit_poly(img.shape,l_nzlane_rowcols)
+    plot_row, right_fit_col, _ = fit_poly(img.shape,r_nzlane_rowcols)
+    img[left_rows, left_cols] = [255, 0, 0]
+    img[right_rows, right_cols] = [0, 0, 255]
+    plt.imshow(img)
+    plt.plot(left_fit_col, plot_row, color='yellow')
+    plt.plot(right_fit_col, plot_row, color='yellow')
+    plt.xlim(0, 1280)
+    plt.ylim(720, 0)
+    plt.show()
