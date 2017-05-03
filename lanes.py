a8a7e0d3f0e0876e59f7597551c2f278bd2e7ffe
Satchel Grant <grantsrb@gmail.com>
2017-05-03 04:14:54 -0700

    remove unnecessary functions

--- lanes.py
+++ lanes.py
@@ -15,7 +15,7 @@ def window_walls(current_column, margin):
     right_wall = current_column + margin
     return left_wall, right_wall
 
-def start_cols(filt_img):
+def find_start_cols(filt_img):
     mid_col = filt_img.shape[1]//2
     search_region = filt_img.shape[0]//2
     # Use histogram on image to find lane base locations
@@ -30,7 +30,7 @@ def find_lane_pixels(filt_img,n_windows=10,margin=100,minpixels=50):
     midpt = output.shape[1]//2
 
     # Use histogram on image to find base locations of lanes
-    left_cols_base, right_cols_base = start_cols(filt_img)
+    left_cols_base, right_cols_base = find_start_cols(filt_img)
 
     # Make windows to isolate potential lane pixels
     window_height = output.shape[0]//n_windows
@@ -184,78 +184,3 @@ def highlight_lanes(l_nzlane_rowcols, r_nzlane_rowcols):
 
 def overlay(img1, img2, portion1=.5, portion2=.5):
     return cv2.addWeighted(img1, portion1, img2, portion2, 0)
-
-
-
-
-################### Unused Functions
-
-def partition_lanes(filtered_img, h_ratio=40/64, w_frac=13, car_hood=0, fill_color=(255,0,0)):
-    ysize, xsize = filtered_img.shape
-    poly_left = np.zeros_like(filtered_img)
-    poly_right = np.zeros_like(filtered_img)
-
-    polyVerticesLeft = np.array([[(0, ysize-car_hood),
-                                (np.floor(w_frac/2)*xsize/w_frac, h_ratio*ysize),
-                                (xsize/2, h_ratio*ysize),
-                                (np.floor(w_frac/2)*xsize/w_frac, ysize-car_hood)]],
-                                dtype=np.int32)
-    polyVerticesRight = np.array([[(np.ceil(w_frac/2)*xsize/w_frac,ysize-car_hood),
-                                    (xsize/2, h_ratio*ysize),
-                                    (np.ceil(w_frac/2)*xsize/w_frac, h_ratio*ysize),
-                                    (xsize, ysize-car_hood)]],
-                                    dtype=np.int32)
-
-    cv2.fillPoly(poly_left, polyVerticesLeft, fill_color)
-    cv2.fillPoly(poly_right, polyVerticesRight, fill_color)
-
-    poly_left = np.logical_and(poly_left,filtered_img)
-    poly_right = np.logical_and(poly_right,filtered_img)
-    return poly_left, poly_right
-
-
-def draw_cv2_lines(img,lines,fill_color=(255,0,0),width=2):
-    for line in lines:
-            for x1,y1,x2,y2 in line:
-                cv2.line(img, (x1, y1), (x2, y2), fill_color, width)
-    return img
-
-def avg_direction(lines):
-    vec = []
-    for line in lines:
-        for x1,y1,x2,y2 in line:
-            dist = math.sqrt((x2-x1)**2+(y2-y1)**2)
-            vec.append([(x2-x1)*dist,(y2-y1)*dist])
-    return np.sum(vec,axis=0)/len(vec)
-
-def ptvecs(lines):
-    pt = (np.sum(lines,axis=0)/len(lines))[0]
-    pt = [(pt[0]+pt[2])/2,(pt[1]+pt[3])/2]
-    vec = avg_direction(lines)
-    return (pt,vec)
-
-def get_line(ptvec,img_shape,h_ratio=40/64):
-    avg_pt = ptvec[0]
-    avg_vec = ptvec[1]
-    slope = avg_vec[1]/avg_vec[0]
-    y1 = img_shape[0]
-    y2 = h_ratio*img_shape[0]
-    x1 = round((y1-avg_pt[1])/slope+avg_pt[0])
-    x2 = round((y2-avg_pt[1])/slope+avg_pt[0])
-    return (int(x1),int(y1),int(x2),int(y2))
-
-def get_lane_lines(filtered_img):
-    filt_left,filt_right = partition_lanes(filtered_img)
-    left_lines = cv2.HoughLinesP(filt_left.astype(np.uint8),
-                                5, np.pi/180, 20, np.array([]))
-    right_lines = cv2.HoughLinesP(filt_right.astype(np.uint8),
-                                5, np.pi/180, 20, np.array([]))
-    leftptvec = ptvecs(left_lines)
-    rightptvec = ptvecs(right_lines)
-    left_line = get_line(leftptvec,filtered_img.shape)
-    right_line = get_line(rightptvec,filtered_img.shape)
-    return left_line,right_line
-
-def draw_lanes(filtered_img):
-    left_line,right_line = get_lane_lines(filtered_img)
-    return draw_cv2_lines(np.zeros_like(filtered_img),[[left_line],[right_line]])
