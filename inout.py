99780a13518d8cfeba58be5ce24bea8bec09adfe
Satchel Grant <grantsrb@gmail.com>
2017-05-03 07:55:01 -0700

    update function description

--- inout.py
+++ inout.py
@@ -1,6 +1,11 @@
 import os
 
 def read_paths(dir_path, file_exts={'jpg':True,'png':True}):
+    # ** Reads in each path from a directory with correct extension to an array **
+
+    # dir_path - string of the path to the directory to read from
+    # file_exts - dict() of desired file extensions
+    
     path_list = []
     for dir_name, subdir, file_list in os.walk(dir_path):
         for f in file_list:
